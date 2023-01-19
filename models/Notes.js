de0c5984df917598ca9f3dfa34e4aa1b587cd0b0
faizan malik <faixan.malik554@gmail.com>
2023-01-19 15:09:06 +0500

    delete endpoint ready

--- models/Notes.js
+++ models/Notes.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 
 const NotesSchema = new mongoose.Schema({
-    noteId: {type: Number, required: true, unique: true , default: Math.floor(Math.random() * 7934582347589234)},
     token: {type: String, required: true},
     title: {type: String, required: true},
     description: {type: String, required: true},
--- pages/api/deletenotes.js
+++ pages/api/deletenotes.js
@@ -1,25 +1,22 @@
 import Notes from '../../models/Notes'
 import connectDb from '../../middleware/mongoose'
-import { useRouter } from 'next/router'
 
-const router = useRouter;
-const handler = async (req,res, context)=>{
+
+
+const handler = async (req,res)=>{
 
     if (req.method == 'DELETE'){
-        console.log(req.body)
-        console.log(context)
-        let note =  await Notes.findById({id: router.query.id})
-        console.log(note)
+        let note =  await Notes.findById(req.query.id)
         if(!note){
          return res.status(404).json({message: 'Note not found'})
         }
 
-        if(note.id != req.body.id){
+        if(note.id != req.query.id){
             return res.status(401).json({message: 'Not authorized'})
         }
 
         else{
-            await Notes.findByIdAndDelete({id: req.body.id})
+            await Notes.findByIdAndDelete(req.query.id)
             return res.status(200).json({ message: 'Note Deleted' })
         } 
 
